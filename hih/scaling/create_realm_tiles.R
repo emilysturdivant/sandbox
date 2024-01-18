38bc67d41077605c4552b3bbcc9d3b7ca487a428
Emily Sturdivant <esturdivant@woodwellclimate.org>
2024-01-18 13:56:02 -0600

    organize HIH scaling scripts and outputs

---
+++ hih/scaling/create_realm_tiles.R
@@ -0,0 +1,64 @@
+#!/usr/bin/env Rscript
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Script:
+#   create_realm_tiles.R
+#
+# Description:
+#   Create shapefile for tiling by continental region in GEE. 
+
+# Author:
+#   Emily Sturdivant - Jan 2024
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+library(tidyverse)
+library(sf)
+sf_use_s2(F)
+library(tmap)
+tmap_mode('view')
+
+# Create shapefile of buffered and diced continental regions ----
+# ~~~~~~~~~~~~~~~~
+# Load continents that were created by buffering World_Continents.shp in QGIS
+continents <- st_read('/Users/esturdivant/data/misc/World_Continents/continents_buffered.geojson') 
+qtm(continents)
+
+# Parameters
+lon_step = 15
+lat_step = 15
+crs_dst <- st_crs('EPSG:4326')
+
+# Create DF with tile corner coordinates
+coords <- tibble(x1=NA, y1=NA, x2=NA, y2=NA,
+                 x3=NA, y3=NA, x4=NA, y4=NA)
+for( lon in seq(-180, 180, lon_step) ){
+  for( lat in seq(-35, 31, lat_step) ){
+    coords <- coords %>% add_row(x1=lon, y1=lat, 
+                                 x2=lon + lon_step, y2=y1, 
+                                 x3=x2, y3=lat + lat_step,
+                                 x4=x1, y4=y3)
+  }
+} 
+
+# Use corner coordinates to create SF polygons
+tiles_sf <- coords %>% 
+  dplyr::filter(!is.na(x1)) %>% 
+  dplyr::rowwise() %>% 
+  dplyr::mutate(geometry = 
+                  sf::st_polygon(
+                    list(matrix(c(x1, y1, x2, y2, x3, y3, x4, y4, x1, y1), 
+                                ncol=2, byrow=TRUE))
+                  ) %>% 
+                  sf::st_sfc(crs=crs_dst)) %>% 
+  sf::st_as_sf(crs=crs_dst) %>% 
+  dplyr::select(-c(x1, y1, x2, y2, x3, y3, x4, y4))
+
+# Intersect with continents
+diced <- tiles_sf %>% 
+  st_intersection(continents) %>% 
+  select(CONTINENT)
+
+## write shapefile to disk ----
+polys_shp <- here::here('/Users/esturdivant/data/misc/World_Continents/tropics_continents_diced.shp')
+diced %>% sf::st_write(polys_shp, delete_dsn=TRUE)
